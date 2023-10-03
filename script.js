954823db69eef4379453fe3a3d900a2f53faaac4
uditlamba <110374158+uditlamba@users.noreply.github.com>
2023-10-03 12:51:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height= parseInt(document.querySelector('#height').value)
+    const weight= parseInt(document.querySelector('#weight').value)
+    const results= document.querySelector('#results')
+
+    
+    if(height===''|| height<0 || isNaN(height)){
+        results.innerHTML='Please give a valid height ';
+
+    }
+    if(weight===''|| weight<0 || isNaN(weight)){
+        results.innerHTML="Please give a valid weight ";
+
+    }
+    else{
+        const Bmi=(weight/((height*height)/10000)).toFixed(2)
+    results.innerHTML=Bmi;
+    }
+    
+})
